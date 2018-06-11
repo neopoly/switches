1cbf649238e530fe3bfd2faae5f7199b3a116ca7
Jonas Thiel <jt@neopoly.de>
2018-06-11 10:52:45 +0200

    Add project stucture

---
+++ source/entry.jsx
@@ -0,0 +1,11 @@
+require("index.html")
+require("stylesheets/style.scss")
+
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "app"
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+)
---
+++ test/app_test.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import App from "app"
+import { assert } from "chai"
+import { mount } from "enzyme"
+
+describe("App component", () => {
+  it("Display headline", () => {
+    const wrapper = mount(<App />)
+
+    assert.strictEqual(wrapper.find("h1").text(), "Insert Switches here.")
+  })
+})
